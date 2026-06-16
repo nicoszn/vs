e9f3c2ac49918d6126c6a9145d367730d8c9523d
nicoszn <141965889+nicoszn@users.noreply.github.com>
2026-06-17 00:39:01 +0100

    Create page.tsx

---
+++ app/check/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LinkChecker from '@/components/LinkChecker';
+
+function Check() {
+  return (
+    <div>
+      <h1>My Link Checker</h1>
+      <LinkChecker />
+    </div>
+  );
+}
+
+export default Check;
